589e291f055ad41bdc032b039e56b52bdff0dbd4
Oluwaseun Adesina <oluwaseunadesina8@gmail.com>
2024-03-13 15:32:01 +0100

    Updated app

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ const upload = multer({ storage: storage });
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Serve static files from the 'uploads' directory
 app.use('/uploads', express.static(uploadPath));
